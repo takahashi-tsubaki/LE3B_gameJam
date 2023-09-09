a422c7b0bdd8972040a01f43ffa3d2d357d881a8
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2023-09-09 21:10:43 +0900

    ぎこちないDashアクションの追加だお。

--- Engine/Player/PlayerManager/PlayerDash.h
+++ Engine/Player/PlayerManager/PlayerDash.h
@@ -13,6 +13,23 @@ public:
 	void Initialize(FbxObject3d* gameObject) override;
 	void Update(Input* input, GamePad* gamePad) override;
 	void Draw()override;
+
+	void Dash();
+
+	void ResetParams();
 private:
+	FbxObject3d* fbxObject_;
+	Vector3 velocity_{ 0,0,0 };
+	float speed = 0.5f;
+	const float MAX_ACCEL = 2.0f;//加速の最大値
+	const float accel = 0.1f;
+	bool isDash_ = false;
+
+	bool isAccel_ = false;
+	bool isDecel_ = false;
+
+	float DashFlameCount = 30.0f;
+
+	float panelCount = 1.0f;
 };
 
